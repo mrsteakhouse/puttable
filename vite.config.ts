dcfed1ce2c126104b43a620a5b4ece662e2c28e7
Fabian Frey <mrsteakhouse1@gmail.com>
2025-06-03 13:06:45 +0200

    feat(*): add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit(), nodePolyfills()],
 });
